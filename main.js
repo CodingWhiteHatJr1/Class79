5bf77473b06456a6ea73016b87d91aa816c5a204
CodingWhiteHatJr1 <83186646+CodingWhiteHatJr1@users.noreply.github.com>
2021-09-01 10:31:08 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,25 +1,46 @@
 Student_list_array = [];
 
 function submit(){
-    var name1=document.getElementById("Name_of_the_students_1").value;
-    var name2=document.getElementById("Name_of_the_students_2").value;
-    var name3=document.getElementById("Name_of_the_students_3").value;
-    var name4=document.getElementById("Name_of_the_students_4").value;
+   var display_Student_list_array = [];
+   for (var j=1; j<=4; j++){
+       var name=document.getElementById("Name_of_the_students_"+j).value;
+       console.log(name);
+       Student_list_array.push(name);
+   }
+   console.log(Student_list_array);
+   var length=Student_list_array.length;
+   console.log(length);
 
-    Student_list_array.push(name1);
-    Student_list_array.push(name2);
-    Student_list_array.push(name3);
-    Student_list_array.push(name4);
-    
-    console.log(Student_list_array);
-    document.getElementById("display_name").innerHTML= Student_list_array;
-    document.getElementById("submit_button").style.display="none";
-    document.getElementById("sort_button").style.display="inline-block";
+   for(var k=0; k<length; k++){
+    display_Student_list_array.push("<h4>NAME- "+Student_list_array[k]+"</h4>");
+    console.log(display_Student_list_array);
+   }
+   console.log(display_Student_list_array);
+   document.getElementById("display_name_with_commas").innerHTML=display_Student_list_array;
+
+   var remove=display_Student_list_array.join(" ");
+   console.log(remove);
+   document.getElementById("display_name_without_commas").innerHTML=remove;
+
+   document.getElementById("submit_button").style.display="none";
+   document.getElementById("sort_button").style.display="inline-block";
 }
-
 function sort(){
-
     Student_list_array.sort();
     console.log(Student_list_array);
-    document.getElementById("display_name").innerHTML= Student_list_array;
+
+    var display_Student_list_array_sort = [];
+    var length=Student_list_array.length;
+    console.log(length);
+
+    for(var k=0; k<length; k++){
+        display_Student_list_array_sort.push("<h4>NAME- "+Student_list_array[k]+"</h4>");
+        console.log(display_Student_list_array_sort);
+    }
+    var remove=display_Student_list_array_sort.join(" ");
+    console.log(remove);
+   document.getElementById("display_name_without_commas").innerHTML=remove;
+}
+function New_Update(){
+    document.getElementById("display_name_without_commas").innerHTML="<h1>"+Student_list_array+"</h1>";
 }
